b1ebcbe33b39ffc3783cbecab7628e43643baff6
Viktor Gavrilov <gavvikal0101@gmail.com>
2026-05-22 18:58:35 +0300

    add logic in task09, 10

--- Task09Project/logic.cpp
+++ Task09Project/logic.cpp
@@ -3,3 +3,18 @@
 //
 // Необходимо спроектировать и реализовать программу, 
 // которая вычисляет сумму цифр заданного числа, кратных 5
+
+int recursion(int number) {
+	if (number == 0) {
+		return 0;
+	}
+
+	int digit = number % 5 == 0 ? number % 10 : 0;
+
+	return recursion(number / 10) + digit;
+}
+
+int sum(int number) {
+	number = number < 0 ? -number : number;
+	return recursion(number);
+}
--- Task10Project/logic.cpp
+++ Task10Project/logic.cpp
@@ -6,6 +6,20 @@
 
 #include "logic.h"
 
+int recursion(int vector[], int size) {
+	if (size == 0) {
+		return 0;
+	}
+
+	int count = vector[size - 1] > 0 ? 1 : 0;
+
+	return recursion(vector, size - 1) + count;
+}
+
 int count_of_positive_elements(int vector[], int size) {
-	return 0;
+	if (size <= 0) {
+		return -1;
+	}
+
+	return recursion(vector, size);
 }
