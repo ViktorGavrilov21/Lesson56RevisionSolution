c36dcdd120967fe6152b085d7fcb3d9eb03ef680
Viktor Gavrilov <gavvikal0101@gmail.com>
2026-05-22 18:16:30 +0300

    add task02, 03, 04

--- Task02Project/logic.cpp
+++ Task02Project/logic.cpp
@@ -5,3 +5,18 @@
 // которая находит n-ное число в последовательности 
 // ряда Фибоначчи с другим началом, где первые два числа равны 1,
 // а каждое следующее – сумме двух предыдущих: 1, 1, 2, 3, 5, 8, 13, …
+
+int recursion(int index) {
+	if (index == 1 || index == 2) {
+		return 1;
+	}
+
+	return recursion(index - 1) + recursion(index - 2);
+}
+
+int fibonacci(int index) {
+	if (index <= 0) {
+		return index;
+	}
+	return recursion(index);
+}
--- Task04Project/logic.cpp
+++ Task04Project/logic.cpp
@@ -3,3 +3,17 @@
 // 
 // Необходимо спроектировать и реализовать программу, 
 // которая возвращает сумму кубов чисел от 1 до N.
+
+int recursion(int number) {
+	if (number == 0) {
+		return 0;
+	}
+	return recursion(number - 1) + number * number * number;
+}
+
+int sum_of_cubes(int number) {
+	if (number <= 0) {
+		return -1;
+	}
+	return recursion(number);
+}
